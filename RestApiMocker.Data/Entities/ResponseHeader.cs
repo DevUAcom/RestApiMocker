ffffc7ef04467a58a124c7246ba57cc5ef58e9b6
nick.codes <nick.codes@outlook.com>
2022-01-14 23:00:14 -0500

    added relationship props b/n responseheader and apprule classes

--- RestApiMocker.Data/Entities/ResponseHeader.cs
+++ RestApiMocker.Data/Entities/ResponseHeader.cs
@@ -4,5 +4,7 @@
     {
         public string Key { get; set; }
         public string Value { get; set; }
+        public AppRule AppRule { get; set; }
+        public int AppRuleId { get; set; }
     }
 }
